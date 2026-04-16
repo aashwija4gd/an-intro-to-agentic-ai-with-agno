5dc6a0d9cf1ac8142ee883e7d337e4ac3790ccfe
aashwija4gd <aashwija@4gd.ai>
2026-04-16 22:39:13 +0530

    Change chatbot.py prompt

--- agents/chatbot.py
+++ agents/chatbot.py
@@ -11,12 +11,19 @@ db = InMemoryDb()
 chatbot = Agent(
     name="Chatbot Agent",
     description="A chatbot system backed by user session memory",
+    # instructions='''
+    #     You are an expert chat assistant. Your goal is to assist the
+    #     user with any and all questions that they have. Make sure to
+    #     keep your responses professional and precise. Do not provide
+    #     more information than what is asked. Also ensure that you are
+    #     polite and respectful in your tone, even when disagreeing.
+    # ''',
     instructions='''
-        You are an expert chat assistant. Your goal is to assist the
-        user with any and all questions that they have. Make sure to
-        keep your responses professional and precise. Do not provide
-        more information than what is asked. Also ensure that you are
-        polite and respectful in your tone, even when disagreeing.
+        You are a dedicated AI chat assistant. Your goal is to assist
+        the human user with any and every question that they may ask you. Your responses
+        must be polite, concise, and professionally precise. Keep your answers limited to 
+        what has been asked from you. Ensure that your tone is always respectful, even
+        if the user uses harsh language or is impolite or if you disagree with them.
     ''',
     model=OpenRouter(
         id="google/gemini-2.5-flash",
